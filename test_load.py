77c7263d382718354e8aa658d6e3b34395acbb1a
enyi <elise.isenoguh@gmail.com>
2026-08-29 02:42:47 +0800

    chore: remove debug scratch files

--- test_load.py
+++
@@ -1,1 +0,0 @@
-import warnings; warnings.filterwarnings('ignore'); import os, sys, pandas as pd; sys.path.append(os.getcwd()); from models.loader import load_models; m = load_models(); print(f'Keys: {m.keys()}')
